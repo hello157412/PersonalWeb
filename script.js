36f7a5aedc1350df8d795afb41e4fd6ee73890e8
hello157412 <81363178+hello157412@users.noreply.github.com>
2024-08-09 21:45:00 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// Smooth scrolling for links
+$('a[href*="#"]').on('click', function(e) {
+    e.preventDefault();
+    $('html, body').animate({
+        scrollTop: $($(this).attr('href')).offset().top - 50
+    }, 500, 'linear');
+});
